6d5dc2affe482712f70d0dce2ae8477996754f87
tomyu <yubochinese@163.com>
2016-11-24 14:04:47 +0800

    admin

--- Blog/src/Blog/wwwroot/js/site.js
+++ Blog/src/Blog/wwwroot/js/site.js
@@ -1,1 +1,17 @@
 ﻿// Write your Javascript code.
+$(document)
+    .ready(function () {
+        //login js
+        $('.ui.selection.dropdown').dropdown();
+        $('.ui.menu .ui.dropdown').dropdown({
+            on: 'hover'
+        });
+
+        //adimn js
+        $('.ui.accordion')
+            .accordion({
+                selector: {
+                    trigger: '.title'
+                }
+            });
+    });
